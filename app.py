5d01452b5ac86dc29371f42baa8ce134bf785c65
1stby <love970120@gmail.com>
2024-01-24 01:39:24 +0800

    add linebot機器人增加回復指令

--- app.py
+++ app.py
@@ -12,16 +12,11 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv('YOUR_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('YOUR_CHANNEL_SECRET'))
-
-# client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
-def generate_response(prompt, role="user"):
-    pass
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
-
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
@@ -31,13 +26,43 @@ def callback():
         abort(400)
 
     return 'OK'
+def generate_response(prompt, role="user"):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "你是一個很有用處的幫手"},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return response.choices[0].message.content
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+    msg = event.message.text.lower()
+    if msg.startswith('/echo ') :
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=msg[6:]))
+    elif msg.startswith('/g '):
+        response = generate_response(msg[3:])
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=response))
+    elif msg.startswith('/t '):
+        response = generate_response("請幫我翻譯正體中文:"+msg[3:])
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=response))
+    elif msg.startswith('/e '):
+        response = generate_response("請幫我翻譯英文:"+msg[3:])
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=response))
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=event.message.text)
+    # )
 
 if __name__ == "__main__":
     app.run(debug=True)
